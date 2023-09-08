a8ca704be4a73d9a78284ba1271f45f37fdcade2
Shawky <amamasa121212@gmail.com>
2023-09-08 13:00:02 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'This program checks the value of the variable n'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	int newL = '\n';
+	int sep = ',';
+	int sp = ' ';
+	int arr[10] = {'0', '1', '2', '3', '4',
+			'5', '6', '7', '8', '9'
+			};
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((char)arr[n]);
+		if (n < 9)
+		{
+			putchar((char)sep);
+			putchar((char)sp);
+		}
+	}
+
+	putchar((char)newL);
+
+	return (0);
+}
