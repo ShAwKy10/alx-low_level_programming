319a47bb03a094d68e5c4669cd87da0d2e16b0b5
Shawky <amamasa121212@gmail.com>
2023-09-08 12:34:57 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,18 +13,15 @@
 int main(void)
 {
 	int n;
-	int newL = '\n';
-	int arr[17] = {'0', '1', '2', '3', '4',
+	char arr[18] = {'0', '1', '2', '3', '4',
 			'5', '6', '7', '8', '9',
 			'a', 'b', 'c', 'd', 'e',
-			'f'};
+			'f', '\n'};
 
-	for (n = 0; n <= 16; n++)
+	for (n = 0; n <= 17; n++)
 	{
-		putchar((char)arr[n]);
+		putchar(arr[n]);
 	}
-
-	putchar((char)newL);
 
 	return (0);
 }
