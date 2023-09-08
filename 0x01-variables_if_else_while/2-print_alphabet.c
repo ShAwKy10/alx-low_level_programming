e9fc2f434dc22ac4808475eb274b63e1f4d7622a
Shawky <amamasa121212@gmail.com>
2023-09-08 08:51:50 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'This program checks the value of the variable n'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char arr[27] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
+
+	for (int n = 0; n <= 26; n++)
+	{
+		putchar(arr[n]);
+	}
+	return (0);
+}
