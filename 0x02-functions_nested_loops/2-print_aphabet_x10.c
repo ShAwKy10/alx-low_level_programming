94e9f9d9c65a43a2d1b828ca8106d4b47bdb265a
Shawky <amamasa121212@gmail.com>
2023-09-12 18:10:33 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_aphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the aphabet lowercase follwed by new line x10
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void);
+{
+	int n;
+	int m;
+	char arr[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+			'h', 'i', 'j', 'k', 'l', 'm', 'n',
+			'o', 'p', 'q', 'r', 's', 't', 'u',
+			'v', 'w', 'x', 'y', 'z'};
+
+	for (m = 0; m <= 10; m++)
+	{
+
+		for (n = 0; n <= 25; n++)
+		{
+			_putchar(arr[n]);
+		}
+
+		_putchar('\n');
+
+	}
+}
