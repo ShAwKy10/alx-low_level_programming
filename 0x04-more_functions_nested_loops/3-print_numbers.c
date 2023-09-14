41737f700be138d5f366713dfc4b8e2486b7c87b
Shawky <amamasa121212@gmail.com>
2023-09-14 16:33:06 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+/**
+ * print_numbers - Prints numbers follwed by new line
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+
+	printf("\n");
+}
