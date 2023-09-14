eef94ca28c94ea84376f18ad2afefd7a59b0734a
Shawky <amamasa121212@gmail.com>
2023-09-14 16:34:52 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,16 @@
 void print_numbers(void)
 {
 	int n;
+	int newL = '\n';
+	int arr[10] = {'0', '1', '2', '3', '4',
+			'5', '6', '7', '8', '9'
+			};
 
 	for (n = 0; n < 10; n++)
 	{
-		printf("%d", n);
+		putchar((char)arr[n]);
 	}
 
-	printf("\n");
+	putchar((char)newL);
+
 }
