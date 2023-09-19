223f0a06a927a0b96ad4d3a43d43a45606536a71
Shawky <amamasa121212@gmail.com>
2023-09-19 14:15:10 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - This function make the value of the input = 98.
+ *
+ * @n: Takes in intiger type address input for function
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
