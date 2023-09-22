bdd7d36e8a807759afd26ed546f16f531c6ee221
Shawky <amamasa121212@gmail.com>
2023-09-22 16:04:02 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,12 +18,21 @@ char *_strcat(char *dest, char *src)
 	{
 		if (dest[i] == '\0')
 		{
+			dest[i] = ' ';
+			i++;
+
 			while (src[j] != '\0')
 			{
 				dest[i] = src[j];
 				j++;
 			}
 		}
+	}
+
+	if (dest[i] != '\0')
+	{
+		i++;
+		dest[i] = '\0';
 	}
 
 	return (dest);
