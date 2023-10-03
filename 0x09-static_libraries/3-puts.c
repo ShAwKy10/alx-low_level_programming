208fae00c5f3a3ba1ccd6790a7003cec898c67b7
Shawky <amamasa121212@gmail.com>
2023-10-02 19:02:14 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
+void _puts(char *str)
+{
+	int i;
+	int printer = 0;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + printer));
+		printer++;
+	}
+
+	_putchar('\n');
+}
