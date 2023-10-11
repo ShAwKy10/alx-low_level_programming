dd55be366ce6f6d53b1a832014da5c1441f2c4ce
Shawky <amamasa121212@gmail.com>
2023-10-11 10:58:11 -0700

    Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory is for 0x0E-structures_typedef tasks
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - A dog basics info
+ *
+ * @name: The name of the dog
+ * @owner: The owner of the dog
+ * @age: The age of the dog
+ *
+ * Description: A dog basics info
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
