c2af58a2be40450880c43b1da2e88f0d06d06e39
Shawky <amamasa121212@gmail.com>
2023-10-11 13:37:32 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,55 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate.
+ *
+ * Return: the length of the string.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src
+ *		including the terminating null byte (\0)
+ *		to the buffer pointed to by dest.
+ * @dest: pointer to the buffer in which we copy the string.
+ * @src: string to be copied.
+ *
+ * Return: the pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - A function that creates a new dog.
  * @name: A variable holds the dog`s name.
  * @age: A variable holds the dog`s age.
@@ -14,16 +63,39 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int lenn;
+	int leno;
 	dog_t *nd;
+
+	nd = malloc(sizeof(dog_t));
 
 	if (nd == NULL)
 	{
+		free(nd);
 		return (NULL);
 	}
 
-	nd->name = name;
+	lenn = _strlen(name);
+	leno = _strlen(owner);
+
+	nd->name = malloc((sizeof(char) * lenn) + 1);
+	nd->owner = malloc((sizeof(char) * leno) + 1);
+
+	if (nd->name == NULL)
+	{
+		free(nd->name);
+		return (NULL);
+	}
+
+	if (nd->owner == NULL)
+	{
+		free(nd->owner);
+		return (NULL);
+	}
+
+	_strcp(nd->name, name);
+	_strcp(nd->owner, owner);
 	nd->age = age;
-	nd->owner = owner;
 
 	return (nd);
 }
