aeaf9800579d69b3f2207f2b035b574e0a255da7
Shawky <amamasa121212@gmail.com>
2023-10-25 04:31:37 +0300

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- *pop_listint - deletes the head node of a linked list
- *@head: head of the list
- *
- *Return: the deleted node data
- */
+*pop_listint - deletes the head node of a linked list
+*@head: head of the list
+*
+*Return: the deleted node data
+*/
 
 int pop_listint(listint_t **head)
 {
