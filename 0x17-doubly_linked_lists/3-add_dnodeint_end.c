48761dab6fd91b8c8d5cd36b54c006f4a6cae4ff
Shawky <amamasa121212@gmail.com>
2023-12-08 01:39:33 +0200

    Updatting 3-add_dnodeint_end.c file

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -30,7 +30,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (new_node);
 	}
 
-	while (!cursor)
+	while (cursor->next != NULL)
 		cursor = cursor->next;
 
 	cursor->next = new_node;
