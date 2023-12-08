c826666a1f2513fb0ef40b5905a77d52037f2d91
Shawky <amamasa121212@gmail.com>
2023-12-07 16:57:35 -0800

    Updatting 7-insert_dnodeint.c file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,11 +45,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		else
 			return (NULL);
 	}
-
 	cur->prev = new;
 	cur = new->prev;
 	cur->next = new;
 	new->n = n;
-
 	return (new);
 }
